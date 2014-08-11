0f071738800c2b327df3da17096b136f4be46dee
ktakagaki <kentaroh.takagaki@gmail.com>
2014-08-11 11:16:02 +0200

    Initial commit

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2014.08.10*)
+
+Paclet[
+  Name -> "NounouW",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
